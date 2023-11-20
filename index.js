5d626aa6c3df5dfe82927876a62fbe26310daf2d
wesley oliveira <wesleyvbdo@gmail.com>
2023-11-20 17:06:02 -0300

    finalizado

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-console.log("oi")
+let hero = "the fool"
+let xp = 100
+let ranking
+
+if (xp <= 1000) 
+{
+    ranking = "ferro"
+
+} else if (xp <= 2000) 
+{
+    ranking = "bronze"
+
+} else if ( xp <= 5000)
+{
+    ranking = "prata"
+
+} else if ( xp <= 7000)
+{
+    ranking = "ouro"
+
+} else if (xp <= 8000) 
+{
+    ranking = "platina"
+
+} else if (xp <= 9000)
+{
+    ranking = "ascendente"
+
+} else if ( xp <= 10000)
+{
+    ranking = "imortal"
+
+} else if (xp >= 10001)
+{
+    ranking = "radiante"
+}
+
+
+console.log("o nosso herói " + hero + ", com seus incríveis " + xp + " de xp, tem o ranking de: " + ranking)
